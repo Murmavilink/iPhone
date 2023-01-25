61a5573db5d836da165ed399d51ed503ca757061
Mur <murmavilink@gmail.com>
2023-01-25 13:25:38 +0600

    smoothScroll functionality done

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,3 +1,20 @@
 export const smoothScroll = () => {
-    console.log('smoothScroll');
+    const links = document.querySelectorAll('.header-menu__list > .header-menu__item > a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            let id = link.getAttribute('href').substring(1);
+            let section = document.getElementById(id);
+
+            if(section) {
+                section.scrollIntoView({
+                    block: "center", 
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+
 };
