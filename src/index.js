c9496436d512cfaa5549b78fe13412551fc9b86c
Mur <murmavilink@gmail.com>
2023-01-26 13:25:37 +0600

    added new module accordeon.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { smoothScroll } from "./modules/menu";
 import { burgerMenu } from "./modules/menu";
 import tabs from "./modules/tabs";
-
+import accordeon from "./modules/accordeon";
 
 smoothScroll();
 burgerMenu();
 tabs();
+accordeon();
