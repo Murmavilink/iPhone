f1fcfc18c41362d27561b35a3d508077043d1c65
Mur <murmavilink@gmail.com>
2023-01-26 22:31:30 +0600

    added new module getData.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import { smoothScroll } from "./modules/menu";
 import { burgerMenu } from "./modules/menu";
 import tabs from "./modules/tabs";
 import accordion from "./modules/accordeon";
+import getData from "./modules/getData";
+
 
 smoothScroll();
 burgerMenu();
 tabs();
 accordion();
+getData();
