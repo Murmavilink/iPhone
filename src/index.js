2cc0aee2a6268c19f91a622d407005ae1a0e395f
Mur <murmavilink@gmail.com>
2023-02-15 16:18:25 +0600

    ready for basket

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import { smoothScroll } from "./modules/menu";
-import { burgerMenu } from "./modules/menu";
 import tabs from "./modules/tabs";
 import accordion from "./modules/accordeon";
 import getData from "./modules/getData";
+
+import { smoothScroll } from "./modules/menu";
+import { burgerMenu } from "./modules/menu";
 import { modal } from "./modules/sendForm";
 import { sendData } from "./modules/sendForm";
-
+import { basket } from "./modules/basket";
 
 smoothScroll();
 burgerMenu();
@@ -14,3 +15,4 @@ accordion();
 getData();
 modal();
 sendData();
+basket();
--- src/modules/basket.js
+++ src/modules/basket.js
@@ -1,5 +1,54 @@
 export const basket = () => {
 
+    const productList = document.querySelector('.cross-sell__list');
+    const basketBlock = document.querySelector('.basket__inner');
+
+    const arrayProducts = [];
+
+    productList.addEventListener('click', (e) => {
+
+        if (e.target.classList.contains('button_buy')) {
+            const product = {};
+
+            const productItem = e.target.closest('.cross-sell__item');
+            const productImage = productItem.querySelector('.cross-sell__image');
+            const productTitle = productItem.querySelector('.cross-sell__title');
+            const productPrice = productItem.querySelector('.cross-sell__price');
+
+
+            product.image = productImage.getAttribute('src');
+            product.title = productTitle.textContent;
+            product.price = productPrice.textContent;
+
+
+            arrayProducts.forEach((product, index) => {
+                if (product.title == productTitle.textContent) {
+                    arrayProducts.splice(index, 1);
+                }
+            });
+
+            arrayProducts.push(product);
+        }
+
+
+        if (arrayProducts.length > 0) {
+            basketBlock.innerHTML = '';
+
+
+            arrayProducts.forEach(product => {
+                basketBlock.insertAdjacentHTML('beforeend', `
+                <article class="basket__item">
+                    <img class="basket__image" src="${product.image}">
+                    <h3 class="basket__title">${product.title}</h3>
+                    <p class="basket__price">${product.price}</p>
+                    <button type="button" class="basket__btn">Купить</button>
+                </article>
+                `);
+            });
+
+        }
+
+    });
 
 };
 
